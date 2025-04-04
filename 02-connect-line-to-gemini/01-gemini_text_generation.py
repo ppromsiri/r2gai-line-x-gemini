07bcc35596d5354f0cacec38486968a694bc3a50
Punsiri Boonyakiat <punsiri.boo@gmail.com>
2025-04-04 16:28:11 +0000

    add local_run and utils

--- 02-connect-line-to-gemini/01-gemini_text_generation.py
+++ 02-connect-line-to-gemini/01-gemini_text_generation.py
@@ -1,6 +1,7 @@
 import os
 import PIL.Image
 from google import genai
+from google.genai import types
 from google.genai.types import Part
 from dotenv import load_dotenv
 
@@ -10,7 +11,10 @@ load_dotenv()
 client = genai.Client(api_key=os.environ["GEMINI_API_KEY"])
 response = client.models.generate_content(
     model="gemini-2.0-flash",
-    contents=["How does AI work?"]
+    contents=["How does AI work?"],
+    config=types.GenerateContentConfig(
+            max_output_tokens=200,
+    ),
 )
 print(response.text)
 
--- 02-connect-line-to-gemini/main.py
+++ 02-connect-line-to-gemini/main.py
@@ -1,7 +1,7 @@
 import os
 import functions_framework
 from dotenv import load_dotenv
-
+import base64
 
 from linebot.v3.exceptions import InvalidSignatureError
 from linebot.v3 import WebhookHandler
@@ -28,13 +28,9 @@ from linebot.v3.messaging import (
 )
 
 load_dotenv()
-from google import genai
-from google.genai import types
 
-
-client = genai.Client(api_key=os.environ["GEMINI_API_KEY"])
-CHANNEL_ACCESS_TOKEN = os.environ["CHANNEL_ACCESS_TOKEN"]
-CHANNEL_SECRET = os.environ["CHANNEL_SECRET"]
+CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
+CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
 
 
 configuration = Configuration(access_token=CHANNEL_ACCESS_TOKEN)
@@ -42,6 +38,12 @@ handler = WebhookHandler(CHANNEL_SECRET)
 api_client = ApiClient(configuration)
 line_bot_api = MessagingApi(api_client)
 line_bot_blob_api = MessagingApiBlob(api_client)
+
+from gemini_service import (
+    generate_text,
+    image_description,
+    document_description
+)
 
 
 @functions_framework.http
@@ -69,10 +71,11 @@ def handle_text_message(event):
     line_bot_api.show_loading_animation(
         ShowLoadingAnimationRequest(chat_id=event.source.user_id)
     )
+    gemini_reponse = generate_text(event.message.text)
     line_bot_api.reply_message(
         ReplyMessageRequest(
             reply_token=event.reply_token,
-            messages=[TextMessage(text=event.message.text)],
+            messages=[TextMessage(text=gemini_reponse)],
         )
     )
 
@@ -109,38 +112,20 @@ def handle_location_message(event):
         )
     )
 
-@handler.add(
-    MessageEvent,
-    message=(ImageMessageContent, VideoMessageContent, AudioMessageContent),
-)
-def handle_content_message(event):
+@handler.add(MessageEvent,message=ImageMessageContent)
+def handle_image_message(event):
     line_bot_api.show_loading_animation_with_http_info(
         ShowLoadingAnimationRequest(chat_id=event.source.user_id)
     )
-    if isinstance(event.message, ImageMessageContent):
-        ftype = "image"
-        message_content = line_bot_blob_api.get_message_content(message_id=event.message.id)
-        b64_image = types.Part.from_bytes(
-            data=message_content,
-            mime_type="image/jpeg"
-        )
-        response = client.models.generate_content(
-            model="gemini-2.0-flash-001",
-            contents=[
-                "What is shown in this image in Thai?",
-                b64_image,
-            ],
-        )
-        print(response.text)
-    elif isinstance(event.message, VideoMessageContent):
-        ftype = "viedio"
-    elif isinstance(event.message, AudioMessageContent):
-        ftype = "audio"
+
+    message_content = line_bot_blob_api.get_message_content(message_id=event.message.id)
+    gemini_reponse = image_description(message_content)
+    
     line_bot_api.reply_message(
         ReplyMessageRequest(
             reply_token=event.reply_token,
             messages=[
-                TextMessage(text=f"Thank for sending {ftype} file"),
+                TextMessage(text=gemini_reponse),
             ],
         )
     )
@@ -148,11 +133,15 @@ def handle_content_message(event):
 
 @handler.add(MessageEvent, message=FileMessageContent)
 def handle_file_message(event):
+    
+    doc_content = line_bot_blob_api.get_message_content(message_id=event.message.id)
+    gemini_reponse = document_description(doc_content)
+
     line_bot_api.reply_message(
         ReplyMessageRequest(
             reply_token=event.reply_token,
             messages=[
-                TextMessage(text="Thank for sending the file."),
+                TextMessage(text=gemini_reponse),
             ],
         )
     )
