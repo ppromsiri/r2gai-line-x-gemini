7d955510acb9d40f61929861d5a62f3aa272078f
Punsiri Boonyakiat <punsiri.boo@gmail.com>
2025-04-04 19:30:28 +0000

    remove unused

--- ggsheet.py
+++
@@ -1,7 +0,0 @@
-import gspread
-
-# First you need access to the Google API. Based on the route you
-# chose in Step 1, call either service_account(), oauth() or api_key().
-gc = gspread.service_account(filename="sa.json")
-sh = gc.open_by_key("1WYLQd84Sbkz-VXOHiJ7B2r7kA2PTqDPi6fgucKQO2aE")
-sh.sheet1.append_row(["Hello", "World!"])
